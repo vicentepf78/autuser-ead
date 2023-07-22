32fd1f1fe832cdb429e1e050149de6b808e20ce2
vicentepf78 <vicentepf78.gmail.com>
2023-07-22 13:23:24 -0300

    API RESTFul para Microservices: Do Básico ao Avançado

--- src/main/java/com/ead/authuser/services/UserService.java
+++ src/main/java/com/ead/authuser/services/UserService.java
@@ -1,6 +1,9 @@
 package com.ead.authuser.services;
 
 import com.ead.authuser.models.UserModel;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
 
 import java.util.List;
 import java.util.Optional;
@@ -19,6 +22,9 @@ public interface UserService {
     boolean existsByUsername(String username);
 
     boolean existsByEmail(String email);
+
+    Page<UserModel> findAll(Specification<UserModel> spec, Pageable pageable);
+
 
 
 }
