a738f9abe1b8b7da91606b567eadd900911924f7
vicentepf78 <vicentepf78.gmail.com>
2023-08-20 16:17:48 -0300

    Microservices Patterns para Comunicação Assíncrona Event-Carried State Transfer Pattern e Broker Pattern

--- src/main/java/com/ead/authuser/controllers/UserCourseController.java
+++ src/main/java/com/ead/authuser/controllers/UserCourseController.java
@@ -1,22 +1,19 @@
 package com.ead.authuser.controllers;
 
 import com.ead.authuser.clients.CourseClient;
-import com.ead.authuser.dtos.CourseDto;
-import com.ead.authuser.dtos.UserCourseDto;
-import com.ead.authuser.models.UserCourseModel;
 import com.ead.authuser.models.UserModel;
-import com.ead.authuser.services.UserCourseService;
 import com.ead.authuser.services.UserService;
 import lombok.extern.log4j.Log4j2;
-import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
 
-import javax.validation.Valid;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -29,36 +26,23 @@ public class UserCourseController {
 
     private final UserService userService;
 
-    private final UserCourseService userCourseService;
-
 
     public UserCourseController(final CourseClient courseClient,
-                                final UserService userService,
-                                final UserCourseService userCourseService) {
+                                final UserService userService) {
         this.courseClient = courseClient;
         this.userService = userService;
-        this.userCourseService = userCourseService;
     }
 
     @GetMapping("/users/{userId}/courses")
-    public ResponseEntity<Page<CourseDto>> getAllCoursesByUser(@PageableDefault(sort = "courseId", direction = Sort.Direction.ASC) final Pageable pageable,
+    public ResponseEntity<Object> getAllCoursesByUser(@PageableDefault(sort = "courseId", direction = Sort.Direction.ASC) final Pageable pageable,
                                                                @PathVariable(value = "userId") final UUID userId){
-        return ResponseEntity.status(HttpStatus.OK).body(courseClient.getAllCoursesByUser(userId, pageable));
-    }
-
-    @PostMapping("/users/{userId}/courses/subscription")
-    public ResponseEntity<Object> saveSubscriptionUserInCourse(@PathVariable(value = "userId") UUID userId,
-                                                               @RequestBody @Valid UserCourseDto userCourseDto){
         Optional<UserModel> userModelOptional = userService.findById(userId);
         if(!userModelOptional.isPresent()){
             return  ResponseEntity.status(HttpStatus.NOT_FOUND).body("User not found.");
         }
-        if(userCourseService.existsByUserAndCourseId(userModelOptional.get(), userCourseDto.getCourseId())){
-            return  ResponseEntity.status(HttpStatus.CONFLICT).body("Error: subscription already exists!");
-        }
-        UserCourseModel userCourseModel = userCourseService.save(userModelOptional.get().convertToUserCourseModel(userCourseDto.getCourseId()));
-        return  ResponseEntity.status(HttpStatus.CREATED).body(userCourseModel);
+        return ResponseEntity.status(HttpStatus.OK).body(courseClient.getAllCoursesByUser(userId, pageable));
     }
+
 
 
 }
--- src/main/java/com/ead/authuser/services/impl/UserServiceImpl.java
+++ src/main/java/com/ead/authuser/services/impl/UserServiceImpl.java
@@ -1,8 +1,6 @@
 package com.ead.authuser.services.impl;
 
-import com.ead.authuser.models.UserCourseModel;
 import com.ead.authuser.models.UserModel;
-import com.ead.authuser.repositories.UserCourseRepository;
 import com.ead.authuser.repositories.UserRepository;
 import com.ead.authuser.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,9 +19,6 @@ public class UserServiceImpl implements UserService {
     @Autowired
     private UserRepository userRepository;
 
-    @Autowired
-    private UserCourseRepository userCourseRepository;
-
     @Override
     public List<UserModel> findAll() {
         return userRepository.findAll();
@@ -37,11 +32,6 @@ public class UserServiceImpl implements UserService {
     @Transactional
     @Override
     public void delete(UserModel userModel) {
-        List<UserCourseModel> userCourseModelList = userCourseRepository.findAllUserCourseIntoUser(userModel.getUserId());
-        if(!userCourseModelList.isEmpty()){
-            userCourseRepository.deleteAll(userCourseModelList);
-        }
-
         userRepository.delete(userModel);
     }
 
